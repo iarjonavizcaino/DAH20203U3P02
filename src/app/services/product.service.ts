5f25b7717b61e03defc53f051b7152e46e4bc914
iarjonavizcaino <iarjona@ittepic.edu.mx>
2020-11-25 18:45:48 -0700

    V0.2 Products

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -10,23 +10,23 @@ export class ProductService {
   constructor() {
     this.products.push({
       name: 'Mouse',
-      photo: 'https://picsum.photos/200/300',
-      description: 'Mouse gamer',
-      price: 100.50
+      photo: 'https://picsum.photos/id/237/300/300',
+      description: 'Gato',
+      price: 50.50
     });
 
     this.products.push({
       name: 'Carro',
-      photo: 'https://picsum.photos/200/300',
-      description: 'Mouse gamer',
+      photo: 'https://picsum.photos/id/238/300/300',
+      description: 'Perro',
       price: 100.50
     });
 
     this.products.push({
       name: 'Perro',
-      photo: 'https://picsum.photos/200/300',
-      description: 'Mouse gamer',
-      price: 100.50
+      photo: 'https://picsum.photos/id/239/300/300',
+      description: 'Tlacuache',
+      price: 200.50
     });
   }
 
